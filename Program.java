9d58dd7eaca176623312c6940e7095eed72aada6
Christopher Frenning <chrifren@ifi.uio.no>
2023-10-14 13:18:56 +0200

    Initial Scaffolding

--- Program.java
+++ Program.java
@@ -2,19 +2,45 @@ import java.net.InetAddress;
 
 import spread.*;
 
-public class Program {
+public class Program implements AdvancedMessageListener {
     public static void main(String[] args) {
         final String UNIQUE_ID = "V13isLmyl";
-        final String IFI_SERVER_IP = "129.240.65.60";
+        final String IFI_SERVER_IP = "129.240.65.59";
         final int IFI_SERVER_PORT = 4803;
+        final String GROUP_NAME = "GROUP7";
 
         try {
 
             SpreadConnection connection = new SpreadConnection();
             connection.connect(InetAddress.getByName(IFI_SERVER_IP), IFI_SERVER_PORT, UNIQUE_ID, true, true);
 
+            SpreadGroup group = new SpreadGroup();
+            group.join(connection, GROUP_NAME);
+
+            SpreadMessage message = new SpreadMessage();
+            message.addGroup(group);
+            message.setFifo();
+            message.setObject("MESSAGE1");
+            connection.multicast(message);
+
+            AdvancedMessageListener listener = new Program();
+            connection.add(listener);
+
         } catch ( Exception e ) {
             System.out.println("Catastrophic failure: " + e.getMessage() + "\n");
         }
+    }
+
+    public void regularMessageReceived(SpreadMessage message) {
+        System.out.println("Regular message received: " + getMessageString(message));
+    }
+
+    public void membershipMessageReceived(SpreadMessage message) {
+        System.out.println("Membership message received: " +  getMessageString(message));
+    }
+
+    String getMessageString(SpreadMessage message) {
+        byte[] data = message.getData();
+        return new String(data);
     }
 }
