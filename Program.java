165dd871f127b6f2c3b95494a9e2b36345396309
Christopher Frenning <chrifren@ifi.uio.no>
2023-10-15 09:36:35 +0200

    Small fixes

--- Program.java
+++ Program.java
@@ -9,7 +9,7 @@ import java.util.UUID;
 
 import spread.*;
 
-public class Replica implements AdvancedMessageListener {
+public class Program implements AdvancedMessageListener {
     static final int SECONDS_BETWEEN_SYNC = 2;
 
     static String serverIp = "127.0.0.1";
@@ -32,7 +32,7 @@ public class Replica implements AdvancedMessageListener {
         try {
             parseCommandLineArguments(args);
             printSettings();
-            Program thisInstance = new Replica();
+            Program thisInstance = new Program();
 
             // interactive or batch?
             Optional<String> commandFileName = parseArguments(args, "file", "f");
@@ -99,7 +99,7 @@ public class Replica implements AdvancedMessageListener {
         }
     }
 
-    public Replica() throws Exception {
+    public Program() throws Exception {
 
         connection = new SpreadConnection();
         connection.connect(InetAddress.getByName(serverIp), serverPort, replicaName, true, true);
